586432b8177b5bd1cf6e1288584fb2ebd98650e2
manikanta <mkr@dct.com>
2024-07-29 20:28:07 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose')
+const configureDB = async()=>{
+    try{
+        const url = process.env.DB_URL || 'mongodb://127.0.0.1:27017'
+        const dbName = process.env.DB_NAME || 'myTravel'
+        const db = await mongoose.connect(`${url}/${dbName}`)
+        console.log('connected to db')
+    }
+    catch(e){
+        console.log('error connecting to DB')
+    }
+}
+
+module.exports = configureDB
